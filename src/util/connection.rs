2b9c12bb8f3166863f5210f34449fa182cee5a59
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2025-11-10 13:25:28 -0700

    Added Connection trait

---
+++ src/util/connection.rs
@@ -0,0 +1,15 @@
+use async_trait::async_trait;
+
+use crate::{gui_commands::UciCommandTrait, util::AsyncReadable};
+
+#[async_trait]
+pub trait Connection {
+    type Err: std::fmt::Debug + Send;
+
+    async fn send<C>(
+        &mut self,
+        cmd: C,
+    ) -> Result<Result<C::Response, <C::Response as AsyncReadable>::Err>, Self::Err>
+    where
+        C: UciCommandTrait;
+}
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,7 @@
 mod async_readable;
+mod connection;
 mod streaming_line_reader;
 
 pub use async_readable::AsyncReadable;
+pub use connection::Connection;
 pub use streaming_line_reader::{StreamingLineReader, StringStreamReader, handle_next_line};
